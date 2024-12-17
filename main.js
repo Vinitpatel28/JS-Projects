fd49c0233a17c9f422393e0b9293cab45a557ca4
Vinitpatel28 <vinitpatelrajeshbhai@gmail.com>
2024-12-17 12:49:44 +0530

    add file

---
+++ main.js
@@ -0,0 +1,11 @@
+let name = "Vinit Patel"
+let exp1 = "Web project on Vacation Planner Pro website"
+let exp2 = "We make a final year project on the cleanerside webapp"
+let expe1 = "Vacation Planner Pro"
+let expe2 = "Cleanersite"
+
+document.getElementById("myH1").textContent = `My Name is ${name}`;
+document.getElementById("ex1").textContent = `I previosuly worked on ${exp1}`;
+document.getElementById("ex2").textContent = `Before that ${exp2}`;
+document.getElementById("e1").textContent = ` ${expe1}`;
+document.getElementById("e2").textContent = `${expe2}`;
